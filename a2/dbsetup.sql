7472fa7357468a4545f4680c49a2269f74748137
Koruja <gustavoalvesbezerra@gmail.com>
2016-06-29 16:27:37 -0400

    A2 - Implements question 2

--- a2/dbsetup.sql
+++ a2/dbsetup.sql
@@ -23,18 +23,31 @@ INSERT INTO book VALUES ('COMP001', 'Starbucks', 2016)
 --
 INSERT INTO wrote VALUES (1, 'COMP001', 1)
 INSERT INTO wrote VALUES (4, 'COMP001', 2)
---
-INSERT INTO publication VALUES ('JRN001', 'JOURNAL 001')
+--INSERTS related to PROCEEDINGS
 INSERT INTO publication VALUES ('PR001', 'PROCEEDING 001')
-INSERT INTO publication VALUES ('OS002', 'Operating System Article in Journal')
-INSERT INTO publication VALUES ('OS161', 'OS161 at Uwaterloo')
---
+INSERT INTO publication VALUES ('OS161a', 'OS161 at Uwaterloo')
+INSERT INTO publication VALUES ('OS161b', 'OS161 the base for OS')
+
 INSERT INTO proceedings VALUES ('PR001', 2014)
---
+
+INSERT INTO article VALUES ('OS161b', 'PR001', 185, 198)
+INSERT INTO article VALUES ('OS161a', 'PR001', 161, 170)
+
+INSERT INTO wrote VALUES (4, 'OS161a', 1)
+INSERT INTO wrote VALUES (4, 'OS161b', 1)
+--INSERTS related to JOURNAL
+INSERT INTO publication VALUES ('JRN001', 'JOURNAL 001')
+INSERT INTO publication VALUES ('OS002', 'Operating System Article in Journal')
+INSERT INTO publication VALUES ('AI001', 'Artificial Intelligence Article')
+INSERT INTO publication VALUES ('DB004', 'Database Management Article')
+
 INSERT INTO journal VALUES ('JRN001', 1, 1, 2015)
---
+
 INSERT INTO article VALUES ('OS002', 'JRN001', 73, 85)
-INSERT INTO article VALUES ('OS161', 'PR001', 161, 170)
---
+INSERT INTO article VALUES ('AI001', 'JRN001', 32, 40)
+INSERT INTO article VALUES ('DB004', 'JRN001', 86, 99)
+
 INSERT INTO wrote VALUES (4, 'OS002', 1)
-INSERT INTO wrote VALUES (4, 'OS161', 1)
+INSERT INTO wrote VALUES (3, 'AI001', 1)
+INSERT INTO wrote VALUES (3, 'DB004', 2)
+INSERT INTO wrote VALUES (4, 'DB004', 1)
